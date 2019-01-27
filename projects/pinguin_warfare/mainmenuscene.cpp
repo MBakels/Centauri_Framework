d491e22fbafd9cd79770216b88a95f8a6271505c
MBakels <marcobakels@live.nl>
2019-01-27 19:50:30 +0100

    Removed the map maker button from menu as it is not yet implemented

--- projects/pinguin_warfare/mainmenuscene.cpp
+++ projects/pinguin_warfare/mainmenuscene.cpp
@@ -6,9 +6,9 @@ MainMenuScene::MainMenuScene() {
 	startButton = new Button(Point2(SWIDTH / 2, SHEIGHT / 2 - 200, 10), "assets/StartGameButton.png");
 	AddChild(startButton);
 
-	// Enter map builder button
-	mapBuilderButton = new Button(Point2(SWIDTH / 2, SHEIGHT / 2, 10), "assets/MapMakerButton.png");
-	AddChild(mapBuilderButton);
+	// Enter map builder button (not yet implemented)
+	//mapBuilderButton = new Button(Point2(SWIDTH / 2, SHEIGHT / 2, 10), "assets/MapMakerButton.png");
+	//AddChild(mapBuilderButton);
 
 	// Quit game button
 	quitButton = new Button(Point2(SWIDTH / 2, SHEIGHT / 2 + 200, 10), "assets/QuitGameButton.png");
@@ -19,8 +19,8 @@ MainMenuScene::~MainMenuScene() {
 	// Removing created objects
 	RemoveChild(startButton);
 	delete startButton;
-	RemoveChild(mapBuilderButton);
-	delete mapBuilderButton;
+	//RemoveChild(mapBuilderButton);
+	//delete mapBuilderButton;
 	RemoveChild(quitButton);
 	delete quitButton;
 }
@@ -37,10 +37,12 @@ void MainMenuScene::Update() {
 	if (startButton->CheckPressed(Point2(GetInput()->GetMouseX(), GetInput()->GetMouseY()))) {
 		SceneMaganger::LoadScene(1);
 	}
+	/*
 	if (mapBuilderButton->CheckPressed(Point2(GetInput()->GetMouseX(), GetInput()->GetMouseY()))) {
 		std::cout << "Map builder" << std::endl;
 		//SceneMaganger::LoadScene(1);
 	}
+	*/
 	if (quitButton->CheckPressed(Point2(GetInput()->GetMouseX(), GetInput()->GetMouseY()))) {
 		GetInput()->ExitApplication();
 	}
